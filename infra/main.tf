dea4959353e7914fc0e8a3a33888b7892a4f7c68
pokam1988 <pokamr@outlook.de>
2025-02-28 13:22:25 +0100

    add infra code and modify github action accordinly to my path

---
+++ infra/main.tf
@@ -0,0 +1,18 @@
+provider "azurerm" {
+  features {}
+}
+
+module "aks" {
+  source             = "modules/aks"
+  cluster_name       = "cloudops-cluster"
+  location          = "East US"
+  resource_group     = "cloudops-rg"
+  node_count         = 3
+  node_size          = "Standard_DS2_v2"
+  enable_monitoring  = true
+}
+
+module "monitoring" {
+  source        = "modules/monitoring"
+  aks_cluster_id = module.aks.id
+}
